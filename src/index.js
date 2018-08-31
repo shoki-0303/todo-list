66326b158a7dc64804acf345746444584a373d83
shoki-0303 <shoki522.33333@gmail.com>
2018-08-31 17:38:53 +0900

    import createStore and Provider

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@ import ReactDOM from 'react-dom';
 import EventsIndex from './components/EventsIndex';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<EventsIndex />, document.getElementById('root'));
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+
+const store = createStore(rootReducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <EventsIndex />
+  </ Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
