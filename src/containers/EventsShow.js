0a3245fbda5e42f39ce3dfb729cd06d4f9ecf8d0
shoki-0303 <shoki522.33333@gmail.com>
2018-08-31 18:25:43 +0900

    routingの設定

--- src/containers/EventsShow.js
+++ src/containers/EventsShow.js
@@ -1,4 +1,4 @@
 import {connect} from 'react-redux'
 import EventsShow from '../components/EventsShow'
 
-connect(null, null)(EventsShow)
+export default connect(null, null)(EventsShow)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import EventsIndex from './containers/EventsIndex';
+import EventsShow from './containers/EventsShow';
+
 import registerServiceWorker from './registerServiceWorker';
+
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
@@ -11,7 +16,13 @@ const store = createStore(rootReducer)
 
 ReactDOM.render(
   <Provider store={store}>
-    <EventsIndex />
-  </ Provider>,
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={EventsIndex} />
+        <Route exact path="/events" component={EventsIndex} />
+        <Route exact path="/events/:id" component={EventsShow} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root'));
 registerServiceWorker();
