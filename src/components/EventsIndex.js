70f7187f13d1438fd4a054fed9ed119c52467e06
shoki-0303 <shoki522.33333@gmail.com>
2018-08-31 17:53:48 +0900

    create table view

--- src/components/EventsIndex.js
+++ src/components/EventsIndex.js
@@ -7,7 +7,22 @@ class EventsIndex extends React.Component {
 
   render() {
     return(
-      <div>EventsIndex</div>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>title</th>
+            <th>body</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>title1</td>
+            <td>body1</td>
+          </tr>
+        </tbody>
+      </table>
       );
   }
 }
