b64bbf92d45b58627e531b14175ef597b87c1e0a
shoki-0303 <shoki522.33333@gmail.com>
2018-08-31 17:29:25 +0900

    initial setting
    不要なファイルの削除およびルートのEventsIndexのveiwを作成

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import EventsIndex from './components/EventsIndex';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<EventsIndex />, document.getElementById('root'));
 registerServiceWorker();
